26ab88cd587611e64a478c32ef32fe98386082a1
beckyydo <47190773+beckyydo@users.noreply.github.com>
2021-05-28 13:58:30 -0400

    Update sql

--- Adventure_Query.sql
+++ Adventure_Query.sql
@@ -11,3 +11,9 @@ WHERE ModifiedDate >= '2001-02-01'
 AND ModifiedDate < '2001-03-01'
 GO
 
+/*Retrieve orders during September 2001 and total due exceed $1000*/
+/*Option 1*/
+SELECT SalesOrderID, OrderDate, TotalDue
+FROM Sales.SalesOrderHeader
+WHERE OrderDate BETWEEN '2001-03-01' AND '2001-03-31'
+
